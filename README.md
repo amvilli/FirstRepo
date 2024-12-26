a87a10d7cbeeee74833f938c6881dec3b64ca3c9
villayat <villayat.ali@proton.me>
2024-12-26 12:03:41 +0530

    First Commit On GIT hub!

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello this is my first repo writtin by me 
+and will post on git after few minutes   and it 
+is not written by AI since this is my first repo 
+so i am doing it this one .. have a good day ;
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+ function sum (num1 , num2){
+     const result  = num1 * num2 ;
+     console.log(result, " Hello  from Git hub!")
+     return result ;
+
+ }
+
+ sum(2 , 4 );
