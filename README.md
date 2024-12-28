9f95324e22db176ad9756d7f68333fe3c1ecd89b
villayat <villayat.ali@proton.me>
2024-12-28 14:23:18 +0530

    Direct commit

--- README.md
+++
@@ -1,4 +0,0 @@
-	Hello this is my first repo written by me 
-	and will post on git after few minutes   and It 
-	is not written by AI since this is my first repo 
-	so I  am doing it this one . Have a good Journey :) ;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
  function sum (num1 , num2){
      const result  = num1 * num2 ;
      console.log(result, " Hello  from Git hub!")
+     console.log("This is to test git branch !")		
      return result ;
 
  }
